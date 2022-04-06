a4e839dd76961edb78c9148371e2de601915f0d4
Sveta <svet.lukyantseva@gmail.com>
2022-04-06 13:12:37 +0300

    feat: install devdepenency

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    const request = require('request');
+    request('https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html', function (error, response, body) {
+        res.send(body);
+    });
 })
 
 app.listen(3000)
