d37cbc51fa82e3925dc20fa62fb7b91c57e3580b
Sveta <svet.lukyantseva@gmail.com>
2022-04-06 14:22:26 +0300

    feat: add randomcolor dependency

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 app.get('/', function (req, res) {
     const request = require('request');
     request('https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html', function (error, response, body) {
+        var html = `<div style="margin: 0 auto; width: 40%">`;
+        var json = JSON.parse(body);
+        html += json.map(function(string) {
+            var color 
+        })
         res.send(body);
     });
 })
