bdbac7565c7466f008ac5fe89b72ce74c0f75fa4
Sveta <svet.lukyantseva@gmail.com>
2022-04-06 16:08:46 +0300

    feat: implement unicorn font style

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const request = require('request');
+const randomColor = require('randomcolor');
+
 const app = express();
 
 app.get('/', function (req, res) {
-    const request = require('request');
-    request('https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html', function (error, response, body) {
-        var html = `<div style="margin: 0 auto; width: 40%">`;
-        var json = JSON.parse(body);
-        html += json.map(function(string) {
-            var color 
-        })
-        res.send(body);
+    request('https://baconipsum.com/api/?type=meat-and-filler', function (error, response, body) {
+        let html = `<div style="margin: 0 auto; width: 80%; font-size: 20px; font-weight: bold">`;
+        let arr = JSON.parse(body);
+        let words = arr.join(" ").split(' ');
+
+        html += words.map(function(word) {
+            let color = randomColor();
+            return `<span style="color: ${color}">${word}</span>`;
+        }).join(" ").toUpperCase();
+
+        html += "</div>";
+        res.send(html);
     });
 })
 
